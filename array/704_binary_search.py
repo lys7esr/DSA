671a0056ed549e3af0ba7916ad179088f238e715
Anshika <inshv1006@gmail.com>
2026-07-16 23:34:55 +0530

    D6

---
+++ array/704_binary_search.py
@@ -0,0 +1,18 @@
+# Binary Search, Easy only
+
+class Solution(object):
+    def search(self, nums, target):
+        low=0
+        high=len(nums)-1
+        while(low<=high):
+            mid=(low+high)/2
+            if(nums[mid]==target):
+                return mid
+            elif (nums[mid]<target):
+                low=mid+1
+            else:
+                high=mid-1
+        return -1
+
+# Time-complexity: O(NlogN)
+# Space-complexity: O(1)
